9ec500ffb8c7f3db540a94d535b0348cdafcb74d
Steven Senkowski <scsenkowski@gmail.com>
2017-11-29 18:10:45 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,8 +3,16 @@ module Memorable
   def self.reset_all
     @@songs.clear
   end
-    
+
   def self.count
     self.all.count
+  end
+
+  def self.count
+    @@artists.count
+  end
+
+  def self.reset_all
+    self.all.clear
   end
 end
